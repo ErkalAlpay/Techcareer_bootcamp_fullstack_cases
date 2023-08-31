404c425992f92907f18d4d61d621ac2960ad66ac
Erkal Alpay <erkalalpay98@hotmail.com>
2023-08-31 17:40:38 +0300

    Spring Initializr

---
+++ FinalCase_ToDo_FullStack/erkalalpay/src/main/java/com/techcareerfullstack/erkalalpay/ErkalalpayApplication.java
@@ -0,0 +1,13 @@
+package com.techcareerfullstack.erkalalpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErkalalpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErkalalpayApplication.class, args);
+	}
+
+}
---
+++ FinalCase_ToDo_FullStack/erkalalpay/src/test/java/com/techcareerfullstack/erkalalpay/ErkalalpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techcareerfullstack.erkalalpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErkalalpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
