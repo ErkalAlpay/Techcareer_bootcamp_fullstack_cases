7c00fe50e3ce22e7a7a52df3c6ee138492ccfb7d
Erkal Alpay <erkalalpay98@hotmail.com>
2023-09-16 18:41:25 +0300

    Update ErkalalpayApplication.java

--- FinalCase_ToDo_FullStack/erkalalpay/src/main/java/com/techcareerfullstack/erkalalpay/ErkalalpayApplication.java
+++ FinalCase_ToDo_FullStack/erkalalpay/src/main/java/com/techcareerfullstack/erkalalpay/ErkalalpayApplication.java
@@ -2,8 +2,12 @@ package com.techcareerfullstack.erkalalpay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class},
+		               scanBasePackages={"com.techcareerfullstack.erkalalpay.Repository"})
+
+
 public class ErkalalpayApplication {
 
 	public static void main(String[] args) {
