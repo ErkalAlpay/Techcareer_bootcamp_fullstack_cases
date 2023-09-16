c5c2a55eff4d2a7ca5e4751daec95e817cb02982
Erkal Alpay <erkalalpay98@hotmail.com>
2023-09-16 18:41:38 +0300

    Update TodoController.java

--- FinalCase_ToDo_FullStack/erkalalpay/src/main/java/com/techcareerfullstack/erkalalpay/Controller/TodoController.java
+++ FinalCase_ToDo_FullStack/erkalalpay/src/main/java/com/techcareerfullstack/erkalalpay/Controller/TodoController.java
@@ -1,7 +1,30 @@
 package com.techcareerfullstack.erkalalpay.Controller;
+import com.techcareerfullstack.erkalalpay.Dto.TodoDTO;
+import com.techcareerfullstack.erkalalpay.Service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping
+@RestController("/index")
 public class TodoController {
+    @Autowired
+    private TodoService todoService;
+
+
+    @PostMapping("/create")
+    public void saveTodo(@RequestBody TodoDTO todo){
+        todoService.save(todo);
+
+    }
+
+    @PostMapping("/update")
+    public TodoDTO updateTodo (String newTodo, Long id){
+
+        todoService.updateTodos(id, newTodo);
+
+        return null;
+    }
+
 }
