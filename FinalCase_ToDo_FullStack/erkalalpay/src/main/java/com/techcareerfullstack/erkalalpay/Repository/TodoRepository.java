e43dfb41e61e3211a50871866d92a094460a51dd
Erkal Alpay <erkalalpay98@hotmail.com>
2023-09-16 18:41:21 +0300

    Update TodoRepository.java

--- FinalCase_ToDo_FullStack/erkalalpay/src/main/java/com/techcareerfullstack/erkalalpay/Repository/TodoRepository.java
+++ FinalCase_ToDo_FullStack/erkalalpay/src/main/java/com/techcareerfullstack/erkalalpay/Repository/TodoRepository.java
@@ -4,6 +4,9 @@ import com.techcareerfullstack.erkalalpay.Entity.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
-public interface TodoRepository extends JpaRepository<Todo, Long> {
+public interface TodoRepository extends JpaRepository <Todo, Long> {
+    Todo getById(Long id);
+
 }
